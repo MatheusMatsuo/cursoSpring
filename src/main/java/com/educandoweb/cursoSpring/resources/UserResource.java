260e3a51f7e32d418a5a9ac8e396e319aa8bef41
Matheus <matheus.matsuo@edu.unifil.br>
2022-02-23 18:21:40 -0300

    h2 DB, teste profile e JPA

--- src/main/java/com/educandoweb/cursoSpring/resources/UserResource.java
+++ src/main/java/com/educandoweb/cursoSpring/resources/UserResource.java
@@ -1,5 +1,8 @@
 package com.educandoweb.cursoSpring.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,8 +15,15 @@ import com.educandoweb.cursoSpring.entities.User;
 public class UserResource {
 	
 	@GetMapping
-	public ResponseEntity<User> findAll(){
+	public ResponseEntity<List<User>> findAll(){
 		User u = new User(1L, "Maria", "maria@gmail.com", "9999999", "12345");
-		return ResponseEntity.ok().body(u);
+		User o = new User(1L, "Joao", "joao@gmail.com", "11111111", "12345");
+		
+		List<User> list = new ArrayList<>();
+		list.add(u);
+		list.add(o);
+		
+		
+		return ResponseEntity.ok().body(list);
 	}
 }
